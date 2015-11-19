3ea53701eaf64d27c8ba8cf2e6d00bc195b5b532
victormf-ufrj <victormf.ufrj@gmail.com>
2015-11-19 18:13:54 -0200

    main.jl está feito. Falta terminar o knn.jl e implementar os métodos de similaridade

--- Lista2_Victor/main.jl
+++ Lista2_Victor/main.jl
@@ -4,11 +4,24 @@ Este código é referente ao exercício 1 da lista 2 da disciplina Tópicos Espe
 Implementar o k-NN
 =#
 
-#USINGs
-Pkg.add("Gadfly")
-using Gadfly
+const k = 5
+
+# Mudando a pasta atual do Julia para a pasta deste arquivo
+cd(dirname(Base.source_path()))
 
 # Lendo as notas do arquivo
 avaliacoes = readdlm("ml-100k/u.data")
 
-#
+# Recupera o maior ID deum usuário
+maxIdUsuario = convert(Int32,maximum(avaliacoes[:,1]))
+maxIdItem = convert(Int32,maximum(avaliacoes[:,2]))
+
+# Gera a matriz UxI
+matriz = zeros(maxIdUsuario,maxIdItem)
+
+for avaliacao = 1:size(avaliacoes,1)
+  matriz[avaliacoes[avaliacao,1],avaliacoes[avaliacao,2]] = avaliacoes[avaliacao,3]
+end
+
+# Chama o método knn passando alguma função de similaridade como parâmetro
+previsao = knn(matriz[1,:],matriz,k,cosseno)
